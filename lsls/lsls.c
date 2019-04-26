4b175bad0d0ab32a66cb822fd0c5d231ad13e1a0
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-26 13:12:03 -0400

    working on lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,13 +9,48 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+     // Parse command line
+    if (argc < 2)
+    {
+        fprintf(stderr, "please enter directory path\n");
+        exit(1);
+    }
+    
+    char *path = argv[1];
+    
+    // Open directory
+    
+    DIR *dir = opendir(path);
+    
+    if (dir == NULL)
+    {
+        fprintf(stdout, "can't open this directory\n");
+        exit(1);
+    }
+    
+    // Repeatly read and print entries
+    
+    struct dirent *ent = readdir(dir);
+    
+    
+    while (ent != NULL)
+    {
+        char path[100];
+        sprintf(path, "%s/%s", argv[1], ent->d_name);
+        struct stat buf;
+        //stat(path, &buf);
+        long long file_size = buf.st_size ;
+        int correct = stat(path, &buf);
+        correct != -1 ? printf("file size is %10lld", file_size) : printf(" ");
+        printf("\t%s\n", ent->d_name);
+        ent = readdir(dir);
+    }
+    
+    
+    // Close directory
+    closedir(dir);
+    
+    
+    return 0;
+ 
 }
